8f3774002999e6b616541d796c662ebf56112863
CherylHenkels <cherylhenkelss@gmail.com>
2024-07-29 11:32:26 -0300

    Design da página de login (css e html feitos)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PaginaLoginComponent } from './pagina-login/pagina-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: PaginaLoginComponent
+      }
+];
