77a041ec1c3f63869647b6379bedd37160fe7c25
CherylHenkels <cherylhenkelss@gmail.com>
2024-08-01 15:55:48 -0300

    layout criado (html + css)

---
+++ src/app/shared/components/menu-lateral/menu-lateral.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menu-lateral',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-lateral.component.html',
+  styleUrl: './menu-lateral.component.css'
+})
+export class MenuLateralComponent {
+
+  constructor(private router: Router) { }
+toggleMenu() {
+throw new Error('Method not implemented.');
+}
+logout() {
+throw new Error('Method not implemented.');
+}
+
+}
