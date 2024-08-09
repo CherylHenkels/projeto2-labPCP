c06c6982f3d9766cd9e8d33d568ad2bfda889d09
CherylHenkels <cherylhenkelss@gmail.com>
2024-08-08 23:38:06 -0300

    Página de cadastro de aluno criada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PaginaLoginComponent } from './pages/pagina-login/pagina-login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CadastroDocenteComponent } from './pages/cadastro-docente/cadastro-docente.component';
+import { CadastroAlunoComponent } from './pages/cadastro-aluno/cadastro-aluno.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'cadastro-docente',
     component: CadastroDocenteComponent
+  },
+  {
+    path: 'cadastro-aluno',
+    component: CadastroAlunoComponent
   },
   // { path: 'inicio', component: PaginaLoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
